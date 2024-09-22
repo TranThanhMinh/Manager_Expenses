c2b5cabcd59a75095a366a1b1c698a7913862672
Minh <minhtt.cdcntt@gmail.com>
2024-09-22 17:35:06 +0700

    update ui

--- src/component/Banner/index.tsx
+++ src/component/Banner/index.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { View } from "react-native";
 
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
 const Banner =()=>{
   //var unitId = "ca-app-pub-3940256099942544/6300978111"
  const unitId ='ca-app-pub-5751638294565515/7484204003'
+ const[isShow,setShow] = useState(false)
     return (
-        <BannerAd
-        unitId={unitId}
-        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
-        requestOptions={{
-          requestNonPersonalizedAdsOnly: true,
-        }}
-      />
+       <View>
+        {  isShow?
+            <BannerAd
+            unitId={unitId}
+            size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+            requestOptions={{
+              requestNonPersonalizedAdsOnly: true,
+            }}
+            onAdLoaded={()=>setShow(true)}
+            onAdFailedToLoad={()=>setShow(false)}
+          />:<View></View>
+        }
+       </View>
+      
     )
 }
 export default Banner;
